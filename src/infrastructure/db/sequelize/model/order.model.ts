0fdae598748883b1cf7ab30756ba3d1eac7ebf8d
Ramon Pacheco <rmpacheco2@gmail.com>
2022-08-17 19:55:33 -0300

    add: impl OrderRepositoryInterface with tests

---
+++ src/infrastructure/db/sequelize/model/order.model.ts
@@ -0,0 +1,43 @@
+import {
+    Table,
+    Model,
+    PrimaryKey,
+    Column,
+    ForeignKey,
+    BelongsTo,
+    HasMany,
+} from "sequelize-typescript";
+import Order from "../../../../domain/entity/order";
+import CustomerModel from "./customer.model";
+import OrderItemModel from "./order-item.model";
+
+@Table({
+    tableName: "orders",
+    timestamps: false,
+})
+export default class OrderModel extends Model {
+    @PrimaryKey
+    @Column
+    declare id: string;
+
+    @ForeignKey(() => CustomerModel)
+    @Column({ allowNull: false })
+    declare customer_id: string;
+
+    @BelongsTo(() => CustomerModel)
+    declare customer: CustomerModel;
+
+    @HasMany(() => OrderItemModel, { onUpdate: "CASCADE", hooks: true })
+    declare items: OrderItemModel[];
+
+    @Column({ allowNull: false })
+    declare total: number;
+
+    toDomainOrder(): Order {
+        return new Order(
+            this.id,
+            this.customer_id,
+            this.items.map(it => it.toDomainOrderItem())
+        )
+    }
+}
---
+++ src/infrastructure/repository/order.repository.ts
@@ -0,0 +1,64 @@
+import Order from "../../domain/entity/order";
+import OrderRepositoryInterface from "../../domain/repository/order-repository.interface";
+import OrderItemModel from "../db/sequelize/model/order-item.model";
+import OrderModel from "../db/sequelize/model/order.model";
+
+export default class OrderRepository implements OrderRepositoryInterface {
+
+  async create(entity: Order): Promise<void> {
+    await OrderModel.create(
+      {
+        id: entity.id,
+        customer_id: entity.customerId,
+        total: entity.total(),
+        items: entity.items.map((item) => ({
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          product_id: item.productId,
+          quantity: item.quantity,
+        })),
+      },
+      {
+        include: [{ model: OrderItemModel }],
+      }
+    );
+  }
+
+  async update(entity: Order): Promise<void> {
+    const sequelize = OrderModel.sequelize;
+    await sequelize.transaction(async (t) => {
+      await OrderItemModel.destroy({
+        where: { order_id: entity.id },
+        transaction: t,
+      });
+      const items = entity.items.map((item) => ({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        product_id: item.productId,
+        quantity: item.quantity,
+        order_id: entity.id,
+      }));
+      await OrderItemModel.bulkCreate(items, { transaction: t });
+      await OrderModel.update(
+        {
+          customer_id: entity.customerId,
+          total: entity.total()
+        },
+        { where: { id: entity.id }, transaction: t }
+      );
+    });
+
+  }
+
+  async find(id: string): Promise<Order> {
+    const order = await OrderModel.findOne({ where: { id }, include: ["items"] })
+    return order.toDomainOrder()
+  }
+
+  async findAll(): Promise<Order[]> {
+    const orders = await OrderModel.findAll({include: ["items"]})
+    return orders.map(orderModel => orderModel.toDomainOrder())
+  }
+}
